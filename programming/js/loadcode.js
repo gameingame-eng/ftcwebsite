a78d2d84fd511d4ef2bfe72df6f89dc6185c4e6f
gameingame-eng <xtremeboi6969@gmail.com>
2026-01-31 18:12:36 -0600

    added random comment

--- programming/js/loadcode.js
+++ programming/js/loadcode.js
@@ -6,7 +6,7 @@ function escapeHtml(text) {
         '"': '&quot;',
         "'": '&#039;'
     };
-    return text.replace(/[&<>"']/g, m => map[m]);
+    return text.replace(/[&<>"']/g, m => map[m]); // hey
 }
 
 async function loadCodeExamples() {
